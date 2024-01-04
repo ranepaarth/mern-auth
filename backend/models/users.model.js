0b731079123336992180964a39ecbc98878a3418
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-04 20:45:08 +0530

    backend: user model with pre save password hashing

---
+++ backend/models/users.model.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
