4268bbe80a4d93b2f229026021a2114bcba2e7aa
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-04 20:28:38 +0530

    backend: DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
 import { errorHandler, notFoundError } from "./middlewares/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
+
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
