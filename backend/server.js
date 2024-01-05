66b5746d95fdaf9a55735be64201949a4b022f96
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-05 21:24:38 +0530

    backend: middlewares for cookies & json/form data

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
@@ -10,6 +11,10 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
