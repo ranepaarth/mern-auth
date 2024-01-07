487d548ce3c9b180fb42a3774f7764ff570576ff
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-07 13:36:11 +0530

    frontend: tailwind, font, react-router-dom & react-icons setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        monte: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
