02cc2fba36c09608b06f5dd01335b1ad11405233
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-13 21:41:49 +0530

    frontend: created App structure and client-side routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
-  return <div className="">App</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "login",
+          element: <LoginPage />,
+        },
+        {
+          path: "register",
+          element: <RegisterPage />,
+        },       
+        {
+          path: "profile",
+          element: <ProfilePage />,
+        },       
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
